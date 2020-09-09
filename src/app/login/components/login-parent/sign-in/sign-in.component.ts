424007dd3bab71bda1463afbad43290d7d666eb3
sajelgoel <sajel_goel@yahoo.com>
2020-09-10 00:56:21 +0530

    email pattern check added for sign-in component, classes added

--- src/app/login/components/login-parent/sign-in/sign-in.component.ts
+++ src/app/login/components/login-parent/sign-in/sign-in.component.ts
@@ -11,6 +11,8 @@ export class SignInComponent implements OnInit {
   @Output() currentPage = new EventEmitter();
   userCreds:Signin = new Signin();
 
+  emailPattern:RegExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
   constructor() {}
 
   ngOnInit(): void {
