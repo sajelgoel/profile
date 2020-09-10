a4a2f04b76ee7338f1d3a07d79547232292224c9
sajelgoel <sajel_goel@yahoo.com>
2020-09-11 00:55:36 +0530

    dummy auth guard added for blog in header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './shared/guards/auth-guards.service';
+import { DummyComponent } from './components/dummy/dummy.component';
 
 
 const routes: Routes = [
   {
     path:'login',
     loadChildren: ()=> import('./login/login.module').then(m=>m.LoginModule)
+  },
+  {
+    path:'blog',
+    canActivate:[AuthGuard],
+    component:DummyComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
+import { AuthGuard } from './shared/guards/auth-guards.service';
+import { DummyComponent } from './components/dummy/dummy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DummyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
